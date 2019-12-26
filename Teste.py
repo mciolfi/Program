ec86f087f535c365795bcd19ed739d5cda56f176
Marcio Ciolfi <44344801+mciolfi@users.noreply.github.com>
2019-12-26 14:42:25 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -1,3 +1,4 @@
+# https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
 # Open dataset
 def arq(name, ndata, types):
     import csv
@@ -19,6 +20,25 @@ model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # Main program
-from numpy import exp, array, random, dot
+from numpy import exp, array, random, dot, loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
 
 listt = array(arq('teste.csv', 8, ';'))  # Define listt as the data inside the file
+X = listt[:,0:1]
+y = listt[:,8]
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=2, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(6, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
